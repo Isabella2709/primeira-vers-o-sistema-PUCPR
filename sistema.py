6a0de40843f81838a715fe9184b3e31897a30a96
Isabella Ribas <isabellards@outlook.com>
2024-06-15 19:12:50 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -21,12 +21,12 @@ while(operacao == "S"):
   if(escolha_menu == 1):
    escolha_pergunta = int(input("====== [ESTUDANTES] MENU DE OPERAÇÕES ====== \n 1. Incluir \n 2. Listar \n 3. Atualizar \n 4. Excluir \n 5. Voltar ao menu inicial"))
    if(escolha_pergunta == 1):
-     estudantes_lista = ["Luana", "João", "Lucas"]
+     
      nome_estudante = str(input("Digite o nome do estudante que voce deseja inserir: "))
      estudantes_lista.append(nome_estudante)
      print("Estudante adicionado com sucesso")
    elif(escolha_pergunta == 2):
-    estudantes_lista = ["Luana", "João", "Lucas"]
+   
     for estudante in estudantes_lista:
                 print(estudante)
 
